04b18875fd0302adb2702b11e476da857a56ce04
Swift420 <61661828+Swift420@users.noreply.github.com>
2021-12-01 23:10:41 +0200

    First Commit

---
+++ ecommerce/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Announcement from '../components/Announcement'
+import Navbar from '../components/Navbar'
+import Slider from "../components/Slider"
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
+function Home() {
+    return (
+        <div>
+            <Announcement />
+            <Navbar></Navbar>
+            <Slider></Slider>
+            <Categories></Categories>
+            <Products></Products>
+            <Newsletter></Newsletter>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default Home
